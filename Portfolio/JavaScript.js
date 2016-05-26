4bae84ed1a74d2d968831cf9ceec1c69b331dfe3
Your Name <you@example.com>
2016-05-26 15:45:15 -0400

    initial commit

---
+++ Portfolio/JavaScript.js
@@ -0,0 +1,19 @@
+﻿//adding jquery function
+function myJumbotron() {
+    var winHeight = $(window).height(50);
+    //wrapper div for height of window
+    $('.wrapper').css({
+        height: winHeight
+    });
+    //vertically alligning jumbotron
+
+    $('.jumbotron').css({
+        marginTop: (winHeight / 2) + 'px'
+    });
+
+    //calling the ready function
+
+    $(document).ready(function () {
+        myJumbotron();
+    });
+}
